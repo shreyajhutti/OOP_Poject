717d9c40872293543c336055e5aa60e92e7d6008
hi <hi@Latitude7390>
2022-12-22 16:52:13 +0530

    Shrijay commit Emptycard

--- Black_Jack_Game/src/Game/Player.java
+++ Black_Jack_Game/src/Game/Player.java
@@ -5,5 +5,27 @@ public class Player {
 	private String name;
 	
 	private Card[] hand = new Card[10];
+	
+	private int numCards;
+	
+	public Player(String aName) {
+		this.name = aName;
+		
+		this.emptyHand();
+	}
 
+	
+	public void emptyHand() {
+		
+		for (Integer card =0; card < 10; card++) {
+			
+			this.hand[card] = null;
+			
+		}
+		
+		this.numCards = 0;
+		
+	}
+	
+	
 }
