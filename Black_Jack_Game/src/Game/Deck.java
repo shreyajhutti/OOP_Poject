40161cf67e2ff80a7de6dd07c1859f5cab9507a9
hi <hi@Latitude7390>
2022-12-22 15:18:56 +0530

    Shrijay,s commit PVShuffle classDeck

--- Black_Jack_Game/src/Game/Deck.java
+++ Black_Jack_Game/src/Game/Deck.java
@@ -1,9 +1,20 @@
 package Game;
 
+import java.util.Random;
+
 public class Deck {
 	
 	private Card[] myCards;
+	
+	
 	private Integer numCards;
+	
+	public Deck() {
+		
+		this(1,false);
+		
+	}
+	
 	
 	public Deck(Integer numDeck, boolean shuffle) {
 		
@@ -11,7 +22,7 @@ public class Deck {
 		this.myCards = new Card[this.numCards];
 		
 		Integer card = 0;
-		
+		 
 		for(Integer deck = 0; deck < numDeck; deck++ ) {
 			
 			for(Integer suit = 0; suit < 4; suit++) {
@@ -25,5 +36,32 @@ public class Deck {
 			}
 		}
 		
+		if (shuffle==true) {
+			this.shuffle();
+		}
+		
+		
 	}
+	
+	public void shuffle() {
+		
+		Random rng = new Random();    //RandomNumberGenerator
+		
+		Card temp;
+		
+		Integer j;
+		for(Integer i =0; i < this.numCards; i++) {
+			
+			j = rng.nextInt(this.numCards);
+			
+			temp = this.myCards[i];
+			this.myCards[i]=this.myCards[j];
+			this.myCards[j]=temp;
+			
+		}
+		
+	}
+	
+	
+	
 }
