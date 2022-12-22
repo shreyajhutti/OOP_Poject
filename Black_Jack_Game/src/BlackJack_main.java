d7b6179208b73796dec17f122ebad25a298432e9
Shreya Jhutti <113163720+shreyajhutti@users.noreply.github.com>
2022-12-22 18:11:08 +0530

    main added - shreya

--- Black_Jack_Game/src/BlackJack_main.java
+++ Black_Jack_Game/src/BlackJack_main.java
@@ -1,12 +1,28 @@
 import Game.Deck;
+import Game.Player;
+import java.util.Scanner;
 
 public class BlackJack_main {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		Deck theDeck = new Deck(1, true);
 		
-		Deck myDeck = new Deck(1, true);
-		myDeck.printDeck(24);
+		Player me = new Player("YOU");
+		Player dealer = new Player("DEALER");
+		
+		me.addCard(theDeck.dealNextCard());
+		dealer.addCard(theDeck.dealNextCard());
+		me.addCard(theDeck.dealNextCard());
+		dealer.addCard(theDeck.dealNextCard());
+		
+		System.out.println("Cards are dealt\n");
+		me.printHand(true);
+		dealer.printHand(false);
+		System.out.println("\n");
+		
+		
 	}
 
 }
