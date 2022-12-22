eb65adacc64d168312d8687eab23f3876eecf5f2
hi <hi@Latitude7390>
2022-12-22 16:42:47 +0530

    shrijay updated main

--- Black_Jack_Game/src/BlackJack_main.java
+++ Black_Jack_Game/src/BlackJack_main.java
@@ -3,7 +3,7 @@ import Game.Deck;
 public class BlackJack_main {
 
 	public static void main(String[] args) {
-		//test 1 shrijay 22/12
+		
 		
 		Deck myDeck = new Deck(1, true);
 		myDeck.printDeck(24);
