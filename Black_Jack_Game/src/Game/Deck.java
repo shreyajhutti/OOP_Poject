a92dca8fb18fa63e6bd4bf09021b355591abbaa9
soniakasar <soniakasar@fedora>
2022-12-22 05:37:47 -0500

    sonia - deck

--- Black_Jack_Game/src/Game/Deck.java
+++ Black_Jack_Game/src/Game/Deck.java
@@ -57,7 +57,20 @@ public class Deck {
 			
 		}
 	}
-	
-	
+	public Card dealNextCard() {
+		Card top = this.myCards[0];
+		
+		for( int c = 1; c < this.numCards; c++) {
+			this.myCards[c-1] = this.myCards[c];
+		}
+		this.myCards[this.numCards-1] = null;
+		
+		this.numCards--;
+		
+		
+		
+		
+		return top;
+	}
 	
 }
