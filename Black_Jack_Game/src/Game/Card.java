8c99f22316ffad6f98c63acf85976f2837d23733
Shreya Jhutti <113163720+shreyajhutti@users.noreply.github.com>
2022-12-22 21:51:57 +0530

    finishing changes

--- Black_Jack_Game/src/Game/Card.java
+++ Black_Jack_Game/src/Game/Card.java
@@ -2,8 +2,8 @@ package Game;
 
 public class Card {
 	
-	private Suit mySuit;        //Diamonds,Hearts,Clubs,Spades
-	private Integer myNumber;   //2,3,4,5,6,7,8,9,ace=1,jack=11,queen=12,king=13
+	private Suit mySuit;        
+	private Integer myNumber;   
 	
 	
 	public Card(Suit aSuit, Integer aNumber) {
