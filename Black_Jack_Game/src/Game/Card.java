0883f9553344f9a1a4bcbbd3decf6c7b791859ff
AASHTHA <AASHTHA@Aastha>
2022-12-22 13:33:24 +0530

    Aastha's commit card class.

--- Black_Jack_Game/src/Game/Card.java
+++ Black_Jack_Game/src/Game/Card.java
@@ -2,13 +2,17 @@ package Game;
 
 public class Card {
 	
-	private Suit mySuit; //Diamonds,Hearts,Clubs,Spades
-	private int myNumber; //1,2,3,4,5,6,7,8,9,ace,jack,queen,king
+	private Suit mySuit;        //Diamonds,Hearts,Clubs,Spades
+	private Integer myNumber;   //2,3,4,5,6,7,8,9,ace=1,jack=11,queen=12,king=13
 	
-	public Card(Suit aSuit, int aNumber) {
+	public Card(Suit aSuit, Integer aNumber) {
 		
 		this.mySuit = aSuit;
 		this.myNumber = aNumber;
 	}
- 
+
+	public Integer getMyNumber() {
+		return myNumber;
+	}
+
 }
--- Black_Jack_Game/src/Game/Suit.java
+++ Black_Jack_Game/src/Game/Suit.java
@@ -1,5 +1,6 @@
 package Game;
 
+//It represents the four possible suits of card.
 public enum Suit {
 	Diamonds,
 	Hearts,
