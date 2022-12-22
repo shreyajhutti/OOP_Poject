d242769aab7163edac8916f1ded1c9985c4824e1
AASHTHA <AASHTHA@Aastha>
2022-12-22 16:26:02 +0530

    Aastha's commit.

--- Black_Jack_Game/src/Game/Deck.java
+++ Black_Jack_Game/src/Game/Deck.java
@@ -60,17 +60,21 @@ public class Deck {
 	public Card dealNextCard() {
 		Card top = this.myCards[0];
 		
-		for( int c = 1; c < this.numCards; c++) {
-			this.myCards[c-1] = this.myCards[c];
+		for( int card = 1; card < this.numCards; card++) {
+			this.myCards[card-1] = this.myCards[card];
 		}
 		this.myCards[this.numCards-1] = null;
 		
 		this.numCards--;
 		
-		
-		
-		
 		return top;
+	}
+	
+	public void printDeck(Integer numToPrint) {
+		for (Integer card=0; card<numToPrint; card++) {
+		    System.out.printf("% 3d/%d %s\n", card+1, this.numCards, this.myCards[card].toString());
+	     }
+	System.out.printf("\t\t[%d other]\n", this.numCards-numToPrint);
 	}
 	
 }
